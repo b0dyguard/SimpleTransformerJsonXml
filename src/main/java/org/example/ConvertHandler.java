cd625ae7b6febe742207cda381e59a81182a4390
b0dyguard <122908413+b0dyguard@users.noreply.github.com>
2026-05-15 00:19:04 +0400

    main commit

--- src/main/java/org/example/ConvertHandler.java
+++ src/main/java/org/example/ConvertHandler.java
@@ -7,7 +7,6 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 
 public class ConvertHandler implements HttpHandler {
     private final ObjectMapper jsonMapper = new ObjectMapper();
